5bacbef77b0ea1e3405038a72eeb39820f97e85f
Tyler Watson <tylerww91@gmail.com>
2022-09-15 09:25:15 -0700

    gets dom elements from html, renames 2 ids in html to match their type

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 
 /* Get DOM Elements */
 
+const cityInput = document.getElementById('city-input');
+const climateSelect = document.getElementById('climate-input');
+const architectureSelect = document.getElementById('architecture-input');
+
+const attractionsInput = document.getElementById('attractions-input');
+
 /* State */
 
 /* Events */
